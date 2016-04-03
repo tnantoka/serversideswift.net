e043b1c55fff9e0a0bbb57ed64de6c14363b06ea
tnantoka <tnantoka@bornneet.com>
2016-04-03 23:35:48 +0900

    Add post 2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,12 @@ let package = Package(
         .Package(url: "https://github.com/qutheory/vapor.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/qutheory/vapor-zewo-mustache.git", majorVersion: 0, minor: 1),
     ],
-    exclude: ["Public", "_posts", "Resources"],
+    exclude: [
+        "Public",
+        "_posts",
+        "Resources",
+        "ansible"
+    ],
     targets: [
         Target(
             name: "SSS"
--- README.md
+++ README.md
@@ -4,13 +4,23 @@
 
 ## Deploy
 
+### DigitalOcean
+
+Create Droplet with Ubuntu 14.04.
+
 ```
+# ~/.ssh/config
 Host sss
-  HostName IP
+  HostName SERVER_IP_ADDRESS
   User root
 ```
 
 ```
 $ ansible-playbook ansible/playbook.yml -i ansible/hosts
 ```
+
+## Acknowledgements
+
+- https://github.com/qutheory/vapor
+- https://github.com/BlackrockDigital/startbootstrap-clean-blog
 
