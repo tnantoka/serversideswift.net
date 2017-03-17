8d4b5a158b06df9c2bffb18c6e70b8c5e5f4a4e8
tnantoka <tnantoka@bornneet.com>
2017-03-18 07:12:12 +0900

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 rm -rf docs
+
+echo 'google_analytics: "UA-75934247-1"' >> _config.yml
+echo 'disqus_shortname: "serversideswift-net"' >> _config.yml
+echo 'ad: 1' >> _config.yml
+
 spelt build --destination docs
 echo "www.serversideswift.net" > docs/CNAME
+
+git checkout _config.yml
+
